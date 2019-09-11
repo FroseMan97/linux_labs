b3f1652b3d1a1aa80764df21cf7d571941472100
sergey lazarev <iazarev@yahoo.com>
2019-09-11 23:33:19 +0300

    done lab1

---
+++ lab1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Программирование в среде Linux\nгр. 6307\nЛазарев Сергей Олегович" << endl;
+    return 0;
+    system("pause");
+}
