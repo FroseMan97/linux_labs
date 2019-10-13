2a370528dd5e46d10507b0c48872276dd4e5b334
sergey lazarev <iazarev@yahoo.com>
2019-10-13 23:39:22 +0300

    update lab 4.3 (fix parallel threading)

--- lab4/4.3/main.cpp
+++ lab4/4.3/main.cpp
@@ -2,41 +2,57 @@
 #include <fstream>
 #include <pthread.h>
 using namespace std;
+
 // 1 поток - нечетные строки
 // 2 поток - четные
-pthread_mutex_t lock;
+
 ofstream out_1, out_2;
- ifstream in;
+ifstream in;
 static void * start_thread_1(void *arg);
 static void * start_thread_2(void *arg);
-string line;
+string line1, line2;
 int main(){
-    pthread_mutex_init(&lock,NULL);
+
     in.open("/home/sergey/Рабочий стол/linux_labs/lab4/4.3/in.txt");
     out_1.open("/home/sergey/Рабочий стол/linux_labs/lab4/4.3/out_1.txt");
     out_2.open("/home/sergey/Рабочий стол/linux_labs/lab4/4.3/out_2.txt");
+
     int linesCount = 0;
+
     if(!(in.is_open() && out_1.is_open() && out_2.is_open()))
         perror("Не удалось открыть файлы");
+
     pthread_t thread_1, thread_2;
     pthread_attr_t attr;
+
     pthread_attr_init(&attr);
-    while(getline(in, line)){
-        pthread_mutex_lock(&lock);
-        linesCount++;
-        if(linesCount % 2 != 0){
-            pthread_create(&thread_1,&attr,&start_thread_1,&line);
-            pthread_join(thread_1,NULL);
+
+    while(true){
+        bool first = true, second = true;
+
+        if(getline(in, line1)){ // считываем строку для 1 потока
+            pthread_create(&thread_1, &attr, &start_thread_1, &line1);
+        }else{
+            first = false;
         }
-        else{
-            pthread_create(&thread_2,&attr,&start_thread_2,&line);
-            pthread_join(thread_2,NULL);
+        if(getline(in, line2)){ // считываем строку для 2 потока
+            pthread_create(&thread_2, &attr, &start_thread_2, &line2);
+        }else{
+            second = false;
         }
-        pthread_mutex_unlock(&lock);
+
+        pthread_join(thread_1, NULL); // ждем 1 поток
+        pthread_join(thread_2, NULL); // ждем 2 поток
+
+        if(!(first && second)){ // если читать больше нечего, выходим из цикла
+            break;
+        }
     }
+
     in.close();
     out_1.close();
     out_2.close();
+
     return 0;
 }
 static void * start_thread_1(void *arg){
@@ -44,6 +60,7 @@ static void * start_thread_1(void *arg){
     out_1 << line << endl;
     pthread_exit(0);
 }
+
 static void * start_thread_2(void *arg){
     string line = *((string *) arg);
     out_2 << line << endl;
